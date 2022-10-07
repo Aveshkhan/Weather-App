310a1d509365ec85ebaf7d5bd3af21263dd7779b
Avesh Khan <muhammad.a@itensia.com>
2022-10-07 21:11:35 +0530

    fixed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
   XRapidAPIAccessKeyHeaderName: 'X-RapidAPI-Key',
   XRapidAPIAccessKeyHeaderValue: '111fe0a9famsh5e1f1ccf7cd2630p14fa27jsn2455bd145019'
 };
